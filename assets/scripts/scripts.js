37076f7af4b8d73f5a13c675a52f01821187a0fa
tomato23d <anna.a.art@gmail.com>
2023-03-24 23:32:45 +1100

    sesrch vin code implemented with the testdata in local array

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,1 +1,44 @@
 var btnSearchVin = document.getElementById("search-vin");
+var btnClearSearch = document.getElementById("clear-search");
+var entryForm = document.getElementById("entry_form");
+
+var inputVin = document.getElementById("vin-input");
+var searchOutput = document.getElementById("search-output");
+
+
+var getCarDetails = function(event){
+    event.preventDefault();
+    var record = 0;
+
+    for (var i=0; i < vinTyreArr.length; i++){
+        var carVin = inputVin.value.trim();
+        var recordVin = vinTyreArr[i].vin;
+
+        
+        if (carVin === recordVin){
+            var tyreDetails = vinTyreArr[i].tyre_measure;
+            record = 1;
+            var h3Output = document.createElement('h3');
+            h3Output.textContent = "Your tyre search fit : " + tyreDetails;
+            searchOutput.appendChild(h3Output);
+        }
+    }
+        noRecord(record);
+        return
+};
+
+function noRecord(record){
+        if (record === 0){
+            var par = document.createElement('p');
+            par.textContent = "Unfortunately, we were not able to find a tyre record for this VIN."
+            searchOutput.appendChild(par);
+        }
+};
+
+function clearSearch(){
+            searchOutput.textContent = " "
+};
+
+entryForm.addEventListener("submit", getCarDetails);
+btnSearchVin.addEventListener("click", getCarDetails);
+btnClearSearch.addEventListener("click",clearSearch);
---
+++ assets/scripts/test_vin_tyre_ar.js
@@ -0,0 +1,20 @@
+
+
+const vinTyreArr = [
+    { array_id: "001",
+      vin: "KMHH351EMLU120842",
+      tyre_measure: "KMHH351EMLU120abc",
+    },
+
+    { array_id: "002",
+      vin: "KMHH351EMLU120843",
+      tyre_measure: "KMHH351EMLU120def",
+    },
+
+   { array_id: "003",
+     vin: "KMHH351EMLU120844",
+     tyre_measure: "KMHH351EMLU120ghi",
+
+},
+]
+
